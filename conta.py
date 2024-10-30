8fc75db68f1dad3fa9d79bc3e1b1f15012e9ba0d
Barbarahayd <barbarahayd@hotmail.com>
2024-10-29 23:59:53 -0300

    refactor(class): make attributes private

--- conta.py
+++ conta.py
@@ -1,27 +1,35 @@
 
-class Conta:
+class ContaBancaria:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo o objeto... {}".format(self))
         self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato (self):
-        print("O saldo é igual a {} do titular {} ".format(self.saldo, self.titular))
+        print("Titular: {}\nSaldo: R$ {:.2f}".format(self.titular, float(self.__saldo)))
 
     def deposito(self, valor):
-        self.saldo = self.saldo + valor
+        self.__saldo = self.__saldo + valor
 
     def saque(self, valor):
-        self.saldo = self.saldo - valor
+        self.__saldo = self.__saldo - valor
 
     def transferencia(self, valor, destino):
         self.saque(valor)
         destino.deposito(valor)
         print("Transferencia de R${} realizado com sucesso".format(valor))
 
+    @property
+    def titular(self):
+        return self.__titular
 
+    @property
+    def limite(self):
+        return float(self.__limite)
 
-
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
