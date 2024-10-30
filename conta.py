e49f8f2a1825519c2b2970ea84af011a8312d217
barbarahayde <barbara.haydee@cotefacil.com>
2024-10-30 16:19:45 -0300

    refactor: add functions to validate saque conditions

--- conta.py
+++ conta.py
@@ -8,19 +8,28 @@ class ContaBancaria:
         self.__saldo = saldo
         self.__limite = limite
 
-    def extrato (self):
-        print("Titular: {}\nSaldo: R$ {:.2f}".format(self.titular, float(self.__saldo)))
-
     def deposito(self, valor):
         self.__saldo = self.__saldo + valor
 
+    # metodo privado para uso dentro da classe apenas
+    def __autoriza_saque(self, valor_saque):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor_saque <= valor_disponivel
+
     def saque(self, valor):
-        self.__saldo = self.__saldo - valor
+        if self.__autoriza_saque(valor):
+            self.__saldo -= valor
+            print(f"Saque de R$ {valor} realizado com sucesso!")
+        else:
+            print("Saldo insuficiente")
 
     def transferencia(self, valor, destino):
         self.saque(valor)
         destino.deposito(valor)
-        print("Transferencia de R${} realizado com sucesso".format(valor))
+        print(f"Transferencia de R$ {valor} realizado com sucesso")
+
+    def set_limite(self, novo_limite):
+        self.__limite = novo_limite
 
     @property
     def titular(self):
@@ -30,6 +39,21 @@ class ContaBancaria:
     def limite(self):
         return float(self.__limite)
 
-    @limite.setter
-    def limite(self, limite):
-        self.__limite = limite
+    @staticmethod
+    def codigo_banco():
+        return '001'
+
+    @staticmethod
+    def codigos_bancos():
+        return {
+            'BB':'001',
+            'Bradesci':'237',
+            'Caixa':'104'
+        }
+
+    @property
+    def extrato(self):
+        return (
+            f"Titular: {self.__titular} | "
+            f"Saldo: R$ {float(self.__saldo):.2f}"
+        )
